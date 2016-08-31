73876863c67f7d7e730c9d693d1308c6d950e793
a409137197 <409137197@qq.com>
2016-08-31 09:41:16 +0800

    first_commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<button onclick="window.location.href = 'https://192.168.1.23/ca.crt';">点击我下载证书</button>
+</body>
+</html>
